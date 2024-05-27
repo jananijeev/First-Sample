666c3e6678b28325a9c1a2b7a85cb7a1f4d243d3
Jananijeev <155942859+jananijeev@users.noreply.github.com>
2024-05-27 20:57:56 +0530

    Update addd.c

--- add.c
+++ add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()    
 
-    int number1, number2, sum;
+    int number3, number2, sum;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
